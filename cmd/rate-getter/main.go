1f1f69c65e2903cd5ff89a9d4569c34ed4bce675
ahaha0807 <b5331@oic.jp>
2018-02-03 00:21:25 +0900

    APIレスポンスをjsonファイルに書き込む形に変更

--- cmd/rate-getter/main.go
+++ cmd/rate-getter/main.go
@@ -1,12 +1,31 @@
 package main
 
 import (
+	"os"
 	"fmt"
+	"encoding/json"
+	"bytes"
 
 	getter "github.com/CryptocurrencyApp/go-coin-rate-getter"
 )
 
+const newestResultFilePath = "./ratelog/newest/newest.json"
+
 func main() {
 	response := getter.Access()
-	fmt.Println(response)
+
+	newestFile, err := os.OpenFile(newestResultFilePath, os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		panic(err)
+	}
+
+	newestResponse, err := json.Marshal(response)
+	if err != nil {
+		panic(err)
+	}
+
+	buffer := new(bytes.Buffer)
+	json.Indent(buffer, newestResponse, "", "    ")
+
+	fmt.Fprintln(newestFile, buffer.String())
 }
