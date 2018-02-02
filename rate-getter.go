85066e9c06a13d2d0e681c33e6b0100b2f42f3a9
ahaha0807 <b5331@oic.jp>
2018-02-02 19:51:46 +0900

    APIリクエストを行う関数の作成
    レスポンスの確認用にPrintlnを使用

--- rate-getter.go
+++ rate-getter.go
@@ -1,7 +1,41 @@
 package rate_getter
 
+import (
+	"net/url"
+	"net/http"
+	"io/ioutil"
+	"fmt"
+)
+
 type response struct {
 }
 
+const baseUrl = "https://api.coinmarketcap.com/v1/ticker"
+const coinTypeCount = 300
+
 func Access() {
+	result := request()
+	fmt.Println(result)
+}
+
+func request() string {
+	option := createOption()
+
+	resp, err := http.Get(baseUrl + "?" + option.Encode())
+	if err != nil {
+		panic(err)
+	}
+
+	result, _ := ioutil.ReadAll(resp.Body)
+
+	defer resp.Body.Close()
+	return string(result)
+}
+
+func createOption() url.Values {
+	value := url.Values{}
+	value.Add("start", "1")
+	value.Add("limit", string(coinTypeCount))
+
+	return value
 }
